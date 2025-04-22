9e691bb2c553ce70c32fc0168f2e011f7118a2ed
Lara Kammerer <lara.kammerer@gmail.com>
2025-04-22 14:01:41 +0200

    Initialer Aufbau mit APIGateway, Load Balancer und BackendNode

---
+++ BackendNode/src/main/java/org/kammerer_neuhold/StatusServerApplication.java
@@ -0,0 +1,12 @@
+package org.kammerer_neuhold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatusServerApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(StatusServerApplication.class, args);
+    }
+}
